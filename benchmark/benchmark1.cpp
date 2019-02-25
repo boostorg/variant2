3d946ecb711cea4012f8af1ad2ed8e72065445b1
Peter Dimov <pdimov@gmail.com>
2019-02-25 18:59:49 +0200

    Add benchmark2.cpp

--- benchmark/benchmark1.cpp
+++ benchmark/benchmark1.cpp
@@ -1,3 +1,10 @@
+// Copyright 2019 Peter Dimov
+//
+// Distributed under the Boost Software License, Version 1.0.
+//
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+
 #if defined(ONLY_V2)
 # define NO_BV
 # define NO_SV
