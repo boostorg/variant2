be6ddf9fdc91d408a3021ee5443f02b3d62d45eb
Peter Dimov <pdimov@gmail.com>
2021-09-11 17:19:09 +0300

    Add boost/variant2.hpp

--- test/quick.cpp
+++ test/quick.cpp
@@ -1,10 +1,8 @@
-
 // Copyright 2019 Peter Dimov.
-//
 // Distributed under the Boost Software License, Version 1.0.
-// http://www.boost.org/LICENSE_1_0.txt
+// https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/variant2/variant.hpp>
+#include <boost/variant2.hpp>
 
 using namespace boost::variant2;
 
